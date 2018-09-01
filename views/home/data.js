b63e39b6c89a4ee0499ba30d2c92a06d0c28f789
zhaojianrun <zhaojianrun@163.com>
2018-09-01 14:28:31 +0800

    轮播图与分类

--- views/home/data.js
+++ views/home/data.js
@@ -1,216 +1,113 @@
 var data = {
-	errCode : '0000',
-	errMsg : '',
-	data : {
-		name : 'asdf',
-		modules : [{
-			type : 'slider',
-			data : [{
-				img : 'http://img.dmall.com/mIndex/201612/f8d1c169-1229-4121-8b39-b997d4cef239_750H',
-				link : '../active/active',
-				selected : true
-			},{
-				img : 'http://img.dmall.com/mIndex/201611/8412c37b-754b-4e1e-8e2c-cb8653cd4d0b_750H',
-				link : '../active/active',
-				selected : false
-			},{
-				img : 'http://img.dmall.com/mIndex/201612/5388bc94-0340-45a6-911d-2e1ea6fd11ca_750H',
-				link : '../active/active',
-				selected : false
-			}]
-		},{
-			type : 'entry',
-			data : [{
-				img : 'http://img.dmall.com/mIndex/201611/396da250-8251-43ca-b4bd-4eaf91dfad8c_240x240H',
-				text : '早晚市',
-				link : '../active/active'
-			},{
-				img : 'http://img.dmall.com/mIndex/201611/6dde70cf-7d85-4fde-ba27-274bb6ab3391_240x240H',
-				text : 'TOP榜',
-				link : '../active/active'
-			},{
-				img : 'http://img.dmall.com/mIndex/201612/85f9a3cc-4755-4244-add0-5dd3b0acb2fd_240x240H',
-				text : '满99减50',
-				link : '../active/active'
-			},{
-				img : 'http://img.dmall.com/mIndex/201611/a9080773-7253-4f4d-81e6-26b67759a90d_240x240H',
-				text : '7折区',
-				link : '../active/active'
-			},{
-				img : 'http://img.dmall.com/mIndex/201611/fe54eeec-71bd-4974-8d7c-b06e13264da3_240x240H',
-				text : '美通卡',
-				link : '../active/active'
-			}]
-		},{
-			type : 'module-3',
-			data : [{
-				img : 'http://img.dmall.com/mIndex/201611/6ef404b8-e474-4614-90d0-c88248c044d6_750H',
-				height : '70',
-				width : '720'
-			}]
-		},{
-			type : 'module-1',
-			data : [{
-				img : 'http://img.dmall.com/20161206/0ec7c6eb-b60a-42d4-acdb-d47ad7844a13_360x360H',
-				title : 'Mixxtail魅夜 专业鸡尾酒 蓝色幻想鸡尾酒（露酒）310ml',
-				price : '8.00',
-				skuId : 123,
-				storeId : 456,
-				originPrice : '9.00',
-				link : '../details/details?sku=123'
-			},{
-				img : 'http://img.dmall.com/20161112/06aacf18-237a-4b70-a238-e64c235007e7_360x360H',
-				title : '曼可顿面包 香蕉牛奶味 68g',
-				price : '1.65',
-				originPrice : '2.25',
-				sku : '',
-				link : '../details/details?sku=456'
-			},{
-				img : 'http://img.dmall.com/20161112/fe0bc645-e1e5-48ef-a792-44a81a2ef66e_360x360H',
-				title : '康师傅蜂蜜绿茶 500ml',
-				price : '2.50',
-				originPrice : '3.00',
-				link : '../details/details?sku=789'
-			},{
-				img : 'http://img.dmall.com/20161112/283de793-f07b-474c-b766-ba1bc2bfa34f_360x360H',
-				title : '金锣玉米热狗肠 40g*10',
-				price : '9.90',
-				originPrice : '12.60',
-				link : '../details/details?sku=789'
-			},{
-				img : 'http://img.dmall.com/20161215/de13f732-009a-4bb5-9b01-27613592d0dd_360x360H',
-				title : '旺仔优酪乳 132g*16/箱',
-				price : '19.90',
-				originPrice : '60.00',
-				link : '../details/details?sku=789'
-			},{
-				img : 'http://img.dmall.com/20161213/b467ed5e-996f-4840-8c3a-600c6dfda9a9_360x360H',
-				title : '绿伞去污超人洗衣液 3kg',
-				price : '38.90',
-				originPrice : '19.45',
-				link : '../details/details?sku=789'
-			}]
-		},{
-			type : 'module-2',
-			data : [{
-				img : 'http://img.dmall.com/mIndex/201612/b29f039d-04c9-4359-b0fd-d9659a80957a_480x480H',
-				link : '../details/details?sku=123'
-			},{
-				img : 'http://img.dmall.com/mIndex/201612/5e872ac3-3980-4747-ad9e-2eaefc9c097c_480x480H',
-				link : '../details/details?sku=456'
-			},{
-				img : 'http://img.dmall.com/mIndex/201612/9e5da637-4ede-4cdb-9d6d-68cdfb0e8728_480x480H',
-				link : '../details/details?sku=789'
-			},{
-				img : 'http://img.dmall.com/mIndex/201612/67343fc3-f05f-40fc-98dd-2e13bff3312e_480x480H',
-				link : '../details/details?sku=789'
-			}]
-		},{
-			type : 'module-1',
-			data : [{
-				img : 'http://img.dmall.com/20161206/0ec7c6eb-b60a-42d4-acdb-d47ad7844a13_360x360H',
-				title : 'Mixxtail魅夜 专业鸡尾酒 蓝色幻想鸡尾酒（露酒）310ml',
-				price : '8.00',
-				skuId : 123,
-				storeId : 456,
-				originPrice : '9.00',
-				link : '../details/details?sku=123'
-			},{
-				img : 'http://img.dmall.com/20161112/06aacf18-237a-4b70-a238-e64c235007e7_360x360H',
-				title : '曼可顿面包 香蕉牛奶味 68g',
-				price : '1.65',
-				originPrice : '2.25',
-				sku : '',
-				link : '../details/details?sku=456'
-			},{
-				img : 'http://img.dmall.com/20161112/fe0bc645-e1e5-48ef-a792-44a81a2ef66e_360x360H',
-				title : '康师傅蜂蜜绿茶 500ml',
-				price : '2.50',
-				originPrice : '3.00',
-				link : '../details/details?sku=789'
-			},{
-				img : 'http://img.dmall.com/20161112/283de793-f07b-474c-b766-ba1bc2bfa34f_360x360H',
-				title : '金锣玉米热狗肠 40g*10',
-				price : '9.90',
-				originPrice : '12.60',
-				link : '../details/details?sku=789'
-			},{
-				img : 'http://img.dmall.com/20161215/de13f732-009a-4bb5-9b01-27613592d0dd_360x360H',
-				title : '旺仔优酪乳 132g*16/箱',
-				price : '19.90',
-				originPrice : '60.00',
-				link : '../details/details?sku=789'
-			},{
-				img : 'http://img.dmall.com/20161213/b467ed5e-996f-4840-8c3a-600c6dfda9a9_360x360H',
-				title : '绿伞去污超人洗衣液 3kg',
-				price : '38.90',
-				originPrice : '19.45',
-				link : '../details/details?sku=789'
-			}]
-		},{
-			type : 'module-3',
-			data : [{
-				img : 'http://img.dmall.com/mIndex/201612/1c81ca94-3c79-453c-bef3-88d89d0c41ff_480x480H',
-				link : '../details/details?sku=234',
-				height : '360',
-				width : '720'
-			},{
-				img : 'http://img.dmall.com/mIndex/201612/1c81ca94-3c79-453c-bef3-88d89d0c41ff_480x480H',
-				link : '../details/details?sku=234',
-				height : '360',
-				width : '720'
-			}]
-		},{
-			type : 'module-3',
-			data : [{
-				img : 'http://img.dmall.com/mIndex/201611/6ef404b8-e474-4614-90d0-c88248c044d6_750H',
-				height : '70',
-				width : '720'
-			}]
-		},{
-			type : 'module-1',
-			data : [{
-				img : 'http://img.dmall.com/20161206/0ec7c6eb-b60a-42d4-acdb-d47ad7844a13_360x360H',
-				title : 'Mixxtail魅夜 专业鸡尾酒 蓝色幻想鸡尾酒（露酒）310ml',
-				price : '8.00',
-				skuId : 123,
-				storeId : 456,
-				originPrice : '9.00',
-				link : '../details/details?sku=123'
-			},{
-				img : 'http://img.dmall.com/20161112/06aacf18-237a-4b70-a238-e64c235007e7_360x360H',
-				title : '曼可顿面包 香蕉牛奶味 68g',
-				price : '1.65',
-				originPrice : '2.25',
-				sku : '',
-				link : '../details/details?sku=456'
-			},{
-				img : 'http://img.dmall.com/20161112/fe0bc645-e1e5-48ef-a792-44a81a2ef66e_360x360H',
-				title : '康师傅蜂蜜绿茶 500ml',
-				price : '2.50',
-				originPrice : '3.00',
-				link : '../details/details?sku=789'
-			},{
-				img : 'http://img.dmall.com/20161112/283de793-f07b-474c-b766-ba1bc2bfa34f_360x360H',
-				title : '金锣玉米热狗肠 40g*10',
-				price : '9.90',
-				originPrice : '12.60',
-				link : '../details/details?sku=789'
-			},{
-				img : 'http://img.dmall.com/20161215/de13f732-009a-4bb5-9b01-27613592d0dd_360x360H',
-				title : '旺仔优酪乳 132g*16/箱',
-				price : '19.90',
-				originPrice : '60.00',
-				link : '../details/details?sku=789'
-			},{
-				img : 'http://img.dmall.com/20161213/b467ed5e-996f-4840-8c3a-600c6dfda9a9_360x360H',
-				title : '绿伞去污超人洗衣液 3kg',
-				price : '38.90',
-				originPrice : '19.45',
-				link : '../details/details?sku=789'
-			}]
-		}]
-	}
+  errCode: '0000',
+  errMsg: '',
+  data: {
+    name: 'home',
+    modules: [{
+        type: 'slider',
+        data: [{
+          img: 'http://pckkf7npd.bkt.clouddn.com/thumbnail/%E6%B6%9B%E5%A3%B0%20%20%E6%9E%97%E3%A6%B2thumbnail.jpg',
+          link: '../active/active',
+          selected: true
+        }, {
+          img: 'http://pckkf7npd.bkt.clouddn.com/thumbnail/%E9%87%8D%E5%B1%B1%E5%A4%8D%E5%B2%AD%20%20%E5%B0%B9%E5%A4%A7%E5%93%B2%20%20thumbnail.jpg',
+          link: '../active/active',
+          selected: false
+        }, {
+          img: 'http://pckkf7npd.bkt.clouddn.com/thumbnail/%E9%87%91%E5%88%9A%E5%B1%B1%20%20%E4%BF%9E%E9%95%BF%20%20%20thumbnail.jpg',
+          link: '../active/active',
+          selected: false
+        }]
+      },
+      {
+        type: 'category',
+        data: [{
+            id: 1,
+            title: '极品推荐', //标题
+            avatar: 'http://pckkf7npd.bkt.clouddn.com/%E6%9E%81%E5%93%81%E6%8E%A8%E8%8D%90.png' //图标
+          },
+          {
+            id: 2,
+            title: '山水风景',
+            avatar: 'http://pckkf7npd.bkt.clouddn.com/%E5%B1%B1%E6%B0%B4%E9%A3%8E%E6%99%AF.png'
+          },
+          {
+            id: 3,
+            title: '花鸟家禽',
+            avatar: 'http://pckkf7npd.bkt.clouddn.com/%E8%8A%B1%E9%B8%9F%E5%AE%B6%E7%A6%BD.png'
+          },
+          {
+            id: 4,
+            title: '飞禽猛兽',
+            avatar: 'http://pckkf7npd.bkt.clouddn.com/%E9%A3%9E%E7%A6%BD%E7%8C%9B%E5%85%BD.png'
+          },
+          {
+            id: 5,
+            title: '民俗民风',
+            avatar: 'http://pckkf7npd.bkt.clouddn.com/%E6%B0%91%E4%BF%97%E6%B0%91%E9%A3%8E.png'
+          },
+          {
+            id: 6,
+            title: '人物肖像',
+            avatar: 'http://pckkf7npd.bkt.clouddn.com/%E4%BA%BA%E7%89%A9%E8%82%96%E5%83%8F.png'
+          },
+          {
+            id: 7,
+            title: '水墨画',
+            avatar: 'http://pckkf7npd.bkt.clouddn.com/%E6%B0%B4%E5%A2%A8%E7%94%BB.png'
+          },
+          {
+            id: 8,
+            title: '静物',
+            avatar: 'http://pckkf7npd.bkt.clouddn.com/%E9%9D%99%E7%89%A9.png'
+          }
+        ]
+      },
+      {
+        type: 'module-1',
+        data: [{
+          img: 'http://img.dmall.com/20161206/0ec7c6eb-b60a-42d4-acdb-d47ad7844a13_360x360H',
+          title: 'Mixxtail魅夜 专业鸡尾酒 蓝色幻想鸡尾酒（露酒）310ml',
+          price: '8.00',
+          skuId: 123,
+          storeId: 456,
+          originPrice: '9.00',
+          link: '../details/details?sku=123'
+        }, {
+          img: 'http://img.dmall.com/20161112/06aacf18-237a-4b70-a238-e64c235007e7_360x360H',
+          title: '曼可顿面包 香蕉牛奶味 68g',
+          price: '1.65',
+          originPrice: '2.25',
+          sku: '',
+          link: '../details/details?sku=456'
+        }, {
+          img: 'http://img.dmall.com/20161112/fe0bc645-e1e5-48ef-a792-44a81a2ef66e_360x360H',
+          title: '康师傅蜂蜜绿茶 500ml',
+          price: '2.50',
+          originPrice: '3.00',
+          link: '../details/details?sku=789'
+        }, {
+          img: 'http://img.dmall.com/20161112/283de793-f07b-474c-b766-ba1bc2bfa34f_360x360H',
+          title: '金锣玉米热狗肠 40g*10',
+          price: '9.90',
+          originPrice: '12.60',
+          link: '../details/details?sku=789'
+        }, {
+          img: 'http://img.dmall.com/20161215/de13f732-009a-4bb5-9b01-27613592d0dd_360x360H',
+          title: '旺仔优酪乳 132g*16/箱',
+          price: '19.90',
+          originPrice: '60.00',
+          link: '../details/details?sku=789'
+        }, {
+          img: 'http://img.dmall.com/20161213/b467ed5e-996f-4840-8c3a-600c6dfda9a9_360x360H',
+          title: '绿伞去污超人洗衣液 3kg',
+          price: '38.90',
+          originPrice: '19.45',
+          link: '../details/details?sku=789'
+        }]
+      }
+    ]
+  }
 }
 
-module.exports = data; 
+module.exports = data;
