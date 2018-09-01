88f862aedbb7517756cff41954dc96e9263fa076
zhaojianrun <zhaojianrun@163.com>
2018-09-01 20:04:47 +0800

    详情页改造

--- pages/details/data.js
+++ pages/details/data.js
@@ -1,49 +1,53 @@
 var data;
 
 data = {
-	cartNum : 12,
-	skuId : 123,
-	storeId : 456,
-	banner : [{
-		img : 'http://img.dmall.com/20150808/25900a1e-4512-4779-9eec-7f93ed97f4c8_800x800H',
-		selected : true
-	},{
-		img : 'http://img.dmall.com/20150808/c9b2cf1a-a494-48c2-ac60-e7d567f31d7d_800x800H',
-		selected : false
-	},{
-		img : 'http://img.dmall.com/20150917/1d09a7ed-ce15-45a9-9618-a8816f77ab57_800x800H',
-		selected : false
-	}],
-	title : '澳洲进口蓝莓 120g/盒 新鲜直达澳洲进口蓝莓 120g/盒',
-	info : '价格已含进口税及邮费',
-	price : '2.30',
-	params : [{
-		name : '产地',
-		value : '其他'
-	},{
-		name : '品种',
-		value : '蓝莓'
-	},{
-		name : '是否有机',
-		value : '否'
-	}],
-	images : [{
-		img : 'http://img.dmall.com/20160708/5e756184-9558-44b3-9775-bb7041676fc2_m640',
-		width : '640',
-		height : '852'
-	},{
-		img : 'http://img.dmall.com/20160708/c93de19c-1324-4e7a-92d1-39b4a87046d2_m640',
-		width : '640',
-		height : '640'
-	},{
-		img : 'http://img.dmall.com/20160708/eed61615-7334-4b25-b913-79626155b37b_m640',
-		width : '640',
-		height : '551'
-	},{
-		img : 'http://img.dmall.com/20160708/2b12832b-1f42-403a-9b51-6331d80454d2_m640',
-		width : '640',
-		height : '696'
-	}]
+  cartNum: 12,
+  skuId: 123,
+  storeId: 456,
+  banner: [{
+    img: 'http://pckkf7npd.bkt.clouddn.com/thumbnail/%E6%B6%9B%E5%A3%B0%20%20%E6%9E%97%E3%A6%B2thumbnail.jpg',
+    selected: true
+  }, {
+    img: 'http://pckkf7npd.bkt.clouddn.com/thumbnail/%E9%87%8D%E5%B1%B1%E5%A4%8D%E5%B2%AD%20%20%E5%B0%B9%E5%A4%A7%E5%93%B2%20%20thumbnail.jpg',
+    selected: false
+  }, {
+    img: 'http://pckkf7npd.bkt.clouddn.com/thumbnail/%E9%87%91%E5%88%9A%E5%B1%B1%20%20%E4%BF%9E%E9%95%BF%20%20%20thumbnail.jpg',
+    selected: false
+  }],
+  title: '江山万里',
+  info: '价格已含进口税及邮费',
+  price: '16800.00',
+  description: '作者运用中国古代山水画的构图编排和叙述方式描绘了水光与天色浑然一体的自然景观，平和安详的宁静气息引人入胜；仿佛悠然自得的一只仙鹤，犹如绚丽多姿的一片霞云。',
+  params: [{
+    name: '画家级别',
+    value: '功勋艺术家'
+  }, {
+    name: '作品材质',
+    value: '布面油画'
+  }, {
+    name: '作品尺寸',
+    value: '160 x 82'
+  }, {
+    name: '创作年代',
+    value: '2017年'
+  }],
+  images: [{
+    img: 'http://img.dmall.com/20160708/5e756184-9558-44b3-9775-bb7041676fc2_m640',
+    width: '640',
+    height: '852'
+  }, {
+    img: 'http://img.dmall.com/20160708/c93de19c-1324-4e7a-92d1-39b4a87046d2_m640',
+    width: '640',
+    height: '640'
+  }, {
+    img: 'http://img.dmall.com/20160708/eed61615-7334-4b25-b913-79626155b37b_m640',
+    width: '640',
+    height: '551'
+  }, {
+    img: 'http://img.dmall.com/20160708/2b12832b-1f42-403a-9b51-6331d80454d2_m640',
+    width: '640',
+    height: '696'
+  }]
 }
 
 module.exports = data;
